232e68d4cabff7af51b5ca864526520849c7aae3
Aryan <bhardwaj7a@gmail.com>
2025-03-13 22:59:52 +0530

    Writing a server component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,30 +1,10 @@
-// Making a client side component , writing code this way will not provide any server side rendering and 
-// it doesn't get rid of the waterfall problem
-
-"use client"
+// Making a server component 
 
 import axios from "axios";
-import { use, useEffect,useState } from "react";
 
-export default function User() {
-    const [loading , setLoading] = useState(true);
-    const [data , setData] = useState();
-
-    useEffect(() => {
-    axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
-    .then(response => {
-        setData(response.data);
-        setLoading(false);
-    })
-    },[])
-
-    if(loading)
-    {
-        return <div>
-            loading--
-        </div>
-    }
-
+export default async function User() {
+   const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+   const data = response.data;
     return <div>
         User Page
         {data.name}
