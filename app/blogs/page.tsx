d6659208748242a08c578973d2eeabf79744df61
Aryan <bhardwaj7a@gmail.com>
2025-03-13 16:49:32 +0530

    Initialised a next project

---
+++ app/blogs/page.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+async function getBlogs() {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos/")
+    return response.data;
+}
+
+export default async function Blogs() {
+    const blogs = await getBlogs();
+
+    return <div>
+        {blogs.map((blog : ITodo , index:number) => <Todo key = {index} title = {blog.title} completed = {blog.completed} /> )}
+    </div>
+}
+
+interface ITodo {
+    title : string;
+    completed : boolean;
+}
+
+function Todo({title,completed} : ITodo) {
+    return <div>
+        {title} {completed ? "done" : "not done"}
+    </div>
+}
