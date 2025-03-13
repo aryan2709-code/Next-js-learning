70adf6e172f0c6324c66ae97625c7e949a7acd1b
Aryan <bhardwaj7a@gmail.com>
2025-03-13 23:42:14 +0530

    Add Loader using loading.tsx

---
+++ app/users/loading.tsx
@@ -0,0 +1,7 @@
+// If there are a bunch of await calls , then i need to have a fallback loading UI. Here it is -- 
+
+export default function Loader() {
+    return <div>
+        Loading---
+    </div>
+}
